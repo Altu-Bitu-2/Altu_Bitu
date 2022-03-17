7ccf620393c1385b4f7794f4fbce9723b8079a82
choiyounji <yunji71@naver.com>
2022-03-17 22:05:42 +0900

    스택,큐,덱-3월11일
    #1213 #11866

---
+++ stackqueuedequeue/ab2.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+//문제1213
+//개수가 홀수개인 알파벳이 두개이상이면 팰린드롬을 만들 수 없다
+
+int main() {
+	string s;
+	int alpha[26] = { 0 };     //알파벳배열
+	cin >> s; //입력
+	for (int i = 0; i < s.length(); i++) {
+		alpha[s[i] - 'A']++;            //알파벳 -'A'를 하면 A=0, .....Z=25가 된다
+	}
+	string ans = "";
+	string tmp = "";       //뒤집은 문자열 저장할 스트링
+	int count=0;
+	int mid;       //가운데 인덱스(홀수개인 알파벳 하나 끼워놓기 위함)
+	for (int i = 0; i < 26; i++) {
+		if (alpha[i] % 2 != 0) {    ///알파벳이 홀수개라는 뜻
+			count++;     //개수가 홀수인 알파벳 개수
+			mid = i;
+		}
+	}
+	if (count > 1) {      //개수가 홀수개인 알파벳이 2개이상일 경우
+		cout << "I'm Sorry Hansoo";
+	}
+	else {
+		for (int i = 0; i < 26; i++) {
+			for (int j = 0; j < alpha[i] / 2; j++) {
+				ans += i + 'A';        //숫자+'A'를 하면 다시 알파벳이 된다
+			}
+		}
+		tmp += ans;
+		reverse(tmp.begin(), tmp.end());    //앞에 문자열을 뒤집어준다
+		if (s.length() % 2 != 0) {
+			ans += mid + 'A';
+			ans += tmp;
+			cout << ans;
+		}
+		else {
+			ans += tmp;
+			cout << ans;
+		}
+	}
+}
---
+++ stackqueuedequeue/ab21.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+//문제11866
+
+int main() {
+	int N,K;
+	cin >>N>>K;  //입력
+	queue<int> que;
+	for (int i = 1; i <= N; i++) {
+		que.push(i);     //1부터 N까지 삽입
+	}
+	cout << '<';
+	while (que.size() != 0) {    //que사이즈가 0이 되기 전까지 계속
+		for (int i = 1; i < K; i++) {      //K번씩 건너뛰는 느낌
+			que.push(que.front());      //i번째 사람을 맨 뒤로 보내
+			que.pop();
+		}
+		cout << que.front();
+		if (que.size() != 1)   //que에 사람이 한명만 남기 전까지
+			cout << ", ";         //띄어쓰기 안해서 백준 틀렸다고 떴었네요..ㅋㅋ
+		que.pop();
+	}
+	cout << '>';
+
+}
