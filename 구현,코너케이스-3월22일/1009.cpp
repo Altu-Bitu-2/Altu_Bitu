a62e42434c9b3d154a14d83622197fbf345725f8
choiyounji <yunji71@naver.com>
2022-03-28 10:52:50 +0900

    구현,코너케이스-3월22일
    #1009 #1316 제출합니다

---
+++ 구현,코너케이스-3월22일/1009.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	int T;
+	vector<int> v;
+	cin >> T;
+	int a, b;
+	int ans;
+	for (int i = 0; i < T; i++) {
+		cin >> a >> b;
+		ans = a%10;
+		for (int i = 1; i < b; i++) {
+			ans *= a;
+			ans %= 10;
+		}
+		if (ans == 0)
+			v.push_back(10);
+		else
+			v.push_back(ans);
+	}
+	for (int i = 0; i < T; i++) {
+		cout << v[i]<<"\n";
+	}
+}
---
+++ 구현,코너케이스-3월22일/1316.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+bool check(string word) {
+	int alphabet[26] = { 0 };
+	int s = word.length();
+	bool ans = true;
+	alphabet[word[0] - 'A'] = 1;
+	for (int i = 1; i < s; i++) {
+		if (word[i] == word[i - 1]) {
+			continue;
+		}
+		if (alphabet[word[i] - 'A'] == 1) {
+			ans = false;
+			break;
+		}
+		alphabet[word[i] - 'A'] = 1;
+	}
+	return ans;
+}
+
+int main() {
+	int t;
+	int countt = 0;
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		string n;
+		cin >> n;
+		if (check(n))
+			countt++;
+	}
+	cout << countt;
+	return 0;
+}
