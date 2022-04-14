4fdf95f0b61a647d3125f57f0eef5fec6c64e747
choiyounji <yunji71@naver.com>
2022-04-14 11:07:55 +0900

    이분탐색-4월8일

---
+++ 이분탐색-4월8일/2840.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	int n, k, s;
+	cin >> n >> k;
+	vector<char> v(n, '?');
+	int cnt = 0;
+	char c;
+	for (int i = 0; i < k; i++) {
+		cin >> s >> c;
+		cnt += s;
+		if (v[cnt % n] == '?')
+			v[cnt % n] = c;
+		else if (v[cnt % n] == c)
+			continue;
+		else {
+			cout << '!';
+			return 0;
+		}
+	}
+	int alphabet[26] = { 0 };
+	for (int i = 0; i < n; i++) {
+		alphabet[v[i] - 'A']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		if (alphabet[i] > 1) {
+			cout << '!';
+			return 0;
+		}
+	}
+	for (int i = cnt % n + n; i >= cnt % n + 1; i--) {
+		cout << v[i % n];
+	}
+
+	return 0;
+}
+
